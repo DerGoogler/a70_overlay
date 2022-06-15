caed47b7ba4aee2497b12d21338d236028013fda
Der_Googler <support@dergoogler.com>
2022-06-15 18:26:55 +0200

    Make spoofs to latest A70 overlay

--- Google/Bonito/Android.mk
+++ Google/Bonito/Android.mk
@@ -1,5 +1,5 @@
 # Generated with generate.sh, made by Der_Googler <support@dergoogler.com>
-# Spoof for Google Bonito. Build date 2022-06-15 / 17:02:23
+# Spoof for Google Bonito. Build date 2022-06-15 / 18:26:29
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
